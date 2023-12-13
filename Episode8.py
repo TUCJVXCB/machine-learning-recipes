167e845fe77ea6f3093ada9e4d378bc6eeab63c3
TUCJVXCB <ttanyuu@hotmail.com>
2023-12-13 23:10:02 +0800

    Episode8

---
+++ Episode8.py
@@ -0,0 +1,23 @@
+training_data = [
+    ['Green', 3, 'Apple'],
+    ['Yellow', 3, 'Apple'],
+    ['Red', 1, 'Grape'],
+    ['Red', 1, 'Grape'],
+    ['Yellow', 3, 'Lemon'],
+]
+
+
+def unique_vals(rows, col):
+    return set(row[col] for row in rows)
+
+
+def class_counts(rows):
+    counts = {}
+    for row in rows:
+        label = row[-1]
+        if label not in counts:
+            counts[label] = 0
+        counts[label] += 1
+    return counts
+
+print(class_counts(training_data))
