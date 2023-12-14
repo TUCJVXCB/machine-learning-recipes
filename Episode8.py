c27d292f88f45064edc9830974d00f5b046171d7
TUCJVXCB <ttanyuu@hotmail.com>
2023-12-14 22:23:19 +0800

    Episode8

--- Episode8.py
+++ Episode8.py
@@ -6,6 +6,8 @@ training_data = [
     ['Yellow', 3, 'Lemon'],
 ]
 
+header = ["color", "diameter", "label"]
+
 
 def unique_vals(rows, col):
     return set(row[col] for row in rows)
@@ -20,4 +22,170 @@ def class_counts(rows):
         counts[label] += 1
     return counts
 
-print(class_counts(training_data))
+
+def is_numeric(value):
+    return isinstance(value, int) or isinstance(value, float)
+
+
+class Question:
+    def __init__(self, column, value):
+        self.column = column
+        self.value = value
+
+    def match(self, example):
+        value = example[self.column]
+        if is_numeric(value):
+            return value >= self.value
+        else:
+            return value == self.value
+
+    def __repr__(self):
+        condition = "=="
+        if is_numeric(self.value):
+            condition = ">="
+        return "Is %s %s %s?" % (header[self.column], condition, str(self.value))
+
+
+def partition(rows, question):
+    true_rows = []
+    false_rows = []
+    for row in rows:
+        if question.match(row):
+            true_rows.append(row)
+        else:
+            false_rows.append(row)
+    return true_rows, false_rows
+
+
+def gini_myself(rows):
+    counts = class_counts(rows)
+    sum = 0
+    for _, v in counts.items():
+        sum += v
+    impurity = 1
+    for _, v in counts.items():
+        impurity -= (v / sum) ** 2
+    return impurity
+
+
+def gini(rows):
+    counts = class_counts(rows)
+    impurity = 1
+    for label in counts:
+        impurity -= (counts[label] / len(counts)) ** 2
+    return impurity
+
+
+lots_of_mixing = [['Apple'],
+                  ['Apple'],
+                  ['Grape'],
+                  ['Grape'],
+                  ['Blueberry']]
+
+
+def info_gain(left, right, current_uncertainty):
+    p = float(len(left)) / (len(left) + len(right))
+    return current_uncertainty - p * gini(left) - (1 - p) * gini(right)
+
+
+def find_best_spilt(rows):
+    best_gain = 0
+    best_question = None
+    current_uncertainty = gini(rows)
+    n_features = len(rows[0]) - 1
+    for col in range(n_features):
+        values = set(row[col] for row in rows)
+
+        for value in values:
+            question = Question(col, value)
+
+            true_rows, false_rows = partition(rows, question)
+            if len(true_rows) == 0 or len(false_rows) == 0:
+                continue
+            gain = info_gain(true_rows, false_rows, current_uncertainty)
+            if gain >= best_gain:
+                best_gain = gain
+                best_question = question
+    return best_gain, best_question
+
+
+class Leaf:
+    def __init__(self, rows):
+        self.predictions = class_counts(rows)
+
+
+class Decision_Node:
+    def __init__(self, question, true_branch, false_branch):
+        self.question = question
+        self.true_branch = true_branch
+        self.false_branch = false_branch
+
+
+def build_tree(rows):
+    gain, question = find_best_spilt(rows)
+    if gain == 0:
+        return Leaf(rows)
+
+    true_rows, false_rows = partition(rows, question)
+    true_branch = build_tree(true_rows)
+    false_branch = build_tree(false_rows)
+    return Decision_Node(question, true_branch, false_branch)
+
+
+def print_tree(node, spacing=""):
+    """World's most elegant tree printing function."""
+
+    # Base case: we've reached a leaf
+    if isinstance(node, Leaf):
+        print(spacing + "Predict", node.predictions)
+        return
+
+    # Print the question at this node
+    print(spacing + str(node.question))
+
+    # Call this function recursively on the true branch
+    print(spacing + '--> True:')
+    print_tree(node.true_branch, spacing + "  ")
+
+    # Call this function recursively on the false branch
+    print(spacing + '--> False:')
+    print_tree(node.false_branch, spacing + "  ")
+
+
+
+def classify(rows, node):
+    if isinstance(node, Leaf):
+        return node.predictions
+    if node.question.match(rows):
+        return classify(rows, node.true_branch)
+    else:
+        return classify(rows, node.false_branch)
+
+
+def print_leaf(counts):
+    """A nicer way to print the predictions at a leaf."""
+    total = sum(counts.values()) * 1.0
+    probs = {}
+    for lbl in counts.keys():
+        probs[lbl] = str(int(counts[lbl] / total * 100)) + "%"
+    return probs
+
+
+if __name__ == '__main__':
+
+    my_tree = build_tree(training_data)
+
+    print_tree(my_tree)
+
+    # Evaluate
+    testing_data = [
+        ['Green', 3, 'Apple'],
+        ['Yellow', 4, 'Apple'],
+        ['Red', 2, 'Grape'],
+        ['Red', 1, 'Grape'],
+        ['Yellow', 3, 'Lemon'],
+    ]
+
+    for row in testing_data:
+        print ("Actual: %s. Predicted: %s" %
+               (row[-1], print_leaf(classify(row, my_tree))))
